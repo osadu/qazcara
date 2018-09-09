271c5204c0d3245fbdead2dfdd5635fb19eee5a5
osadu <oljas.saduakhas@gmail.com>
2018-09-10 01:35:41 +0600

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	$(document).ready(function(){
+     $(".owl-carousel").owlCarousel({
+     	items: 1,
+     	loop: true,
+     	autoplay: true,
+     	//autoplayHoverPause: true,
+     });
+    });
+
+    $( "#tabs" ).tabs();
+
+});
